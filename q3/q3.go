f42e4456ceceb44902269724d8bc275ab64cf21a
LÍVIA LEMOS <122068486+PETROMYZONMONSTER@users.noreply.github.com>
2023-06-18 15:48:24 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -1,9 +1,19 @@
 package q3
 
+import "fmt"
+
 //Você está subindo uma escada. Leva n passos para chegar ao topo.
 //
 //A cada vez, você pode subir 1 ou 2 degraus. De quantas maneiras distintas você pode subir até o topo?
 
 func ClimbStairs(n int) int {
-	return 0
+	if n <= 2 {
+		return n
+	}
+	degraus := make(map[int]int, n+1)
+	for i := 3; i <= n; i++ {
+		degraus[i] = degraus[i-1] + degraus[i-2]
+	}
+	fmt.Println(degraus)
+	return degraus[n]
 }
